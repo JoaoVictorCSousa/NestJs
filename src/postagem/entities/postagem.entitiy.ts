586d4c44689594ada9394908e6339b56dd84b86c
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2022-10-11 16:22:30 -0300

    Ex

---
+++ src/postagem/entities/postagem.entitiy.ts
@@ -0,0 +1,33 @@
+import { applyDecorators } from "@nestjs/common/decorators/core/apply-decorators";
+import { IsNotEmpty } from "class-validator";
+import { Tema } from "src/tema/entities/tema.entities";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+ @Entity({name: 'tb_postagens'})
+ export class Postagem{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    titulo: string;
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    texto: string;
+
+    @UpdateDateColumn()
+    data: Date;
+
+    @ManyToOne(() => Tema, (tema) => tema.postagem, {
+      onDelete : "CASCADE"
+    })
+    tema: Tema;
+
+
+    
+    
+
+    
+ }
